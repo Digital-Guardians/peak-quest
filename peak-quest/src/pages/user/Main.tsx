270008134e272b8207d2fb4c0004f5296bbb16a0
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-06-16 15:06:23 +0900

    [feat] setcion 컴포넌트 통합

--- peak-quest/src/pages/user/Main.tsx
+++ peak-quest/src/pages/user/Main.tsx
@@ -4,11 +4,12 @@ import courseList from "../../assets/course-lists.png";
 import { Link } from "react-router-dom";
 import RankSection from "./RankSection";
 import EventBanner from "./EventBanner";
-        
+import Nearby from "../../components/user/Nearby";
+
 export default function Main() {
   return (
-    <div className="bg-gray px-4 text-black">
-      <div className="flex py-5 items-center text-xl md:text-lg md:leading-4 sm:text-md sm:leading-3 font-bold leading-7">
+    <div className="bg-gray text-black">
+      <div className="flex py-5 justify-center items-center text-xl md:text-lg md:leading-4 sm:text-md sm:leading-3 font-bold leading-7">
         <Link to={"/area"}>
           <div className="mr-2 relative">
             <p className="absolute top-3 left-3">
@@ -34,12 +35,13 @@ export default function Main() {
           </div>
         </Link>
       </div>
-        {/* rankSection */}
+      {/* rankSection */}
       <div className="w-[430px] m-auto bg-white rounded-xl">
         <RankSection />
         <EventBanner />
+        {/* nearbySection */}
+        <Nearby />
       </div>
-     </div>
+    </div>
   );
 }
-
