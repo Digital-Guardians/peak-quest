be2f187a6d95df67e1318420ae0ea2e21810c5a9
Sungmin Hong <sm98310@gmail.com>
2023-06-17 18:16:25 +0900

    [feat] 페이지에 따른 header 스타일 변경

--- peak-quest/src/components/Header.tsx
+++ peak-quest/src/components/Header.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { BsFillPersonFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Header() {
+  // 코스목록 페이지 header 변경
+  const { AreaName } = useParams();
+
   return (
-    <div className="text-black h-[48px] flex justify-between items-center px-[16px]">
+    <div
+      className={
+        AreaName
+          ? "w-full max-w-[430px] text-white h-[48px] flex justify-between items-center px-[16px] fixed top-0 z-50"
+          : "text-black h-[48px] flex justify-between items-center px-[16px]"
+      }
+    >
       <Link to={"/"}>
         <div>logo</div>
       </Link>
