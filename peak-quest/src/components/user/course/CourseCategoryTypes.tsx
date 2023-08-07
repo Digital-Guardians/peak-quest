e92fc68f8eee878370d1c17c8a10842515cab95d
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-08-07 21:56:47 +0900

    [fix] 코스 카테고리 option 값 변경

--- peak-quest/src/components/user/course/CourseCategoryTypes.tsx
+++ peak-quest/src/components/user/course/CourseCategoryTypes.tsx
@@ -8,27 +8,27 @@ export interface courseCategoryOption {
 // 데이터 보내줄 때 id 변경할 것
 export const courseCategoryOptions = [
   {
-    id: "one",
+    id: "혼자서도 갈 수 있는",
     label: "혼자서도 갈 수 있는",
     description: "같이 갈 사람 없어도 걱정 마세요.",
   },
   {
-    id: "two",
+    id: "반려동물과 함께 갈 수 있는",
     label: "반려동물과 함께 갈 수 있는",
     description: "산책겸 운동까지 일석이조!",
   },
   {
-    id: "three",
+    id: "당일치기 가능한",
     label: "당일치기 가능한",
     description: "마음은 가볍게, 짐도 가볍게!",
   },
   {
-    id: "four",
+    id: "자전거 타고 갈 수 있는",
     label: "자전거 타고 갈 수 있는",
     description: "튼튼한 하체, 준비되셨나요?",
   },
   {
-    id: "five",
+    id: "편의시설이 있는",
     label: "편의시설이 있는",
     description: "트래킹 중 화장실 또는 음식점 이용이 가능해요.",
   },
